9ac8098e934548a0d6616e34243fbf397b762f6a
Mutescu Andrei-Ioan <amutescu@yahoo.com>
2021-03-05 09:26:33 +0200

    Abstract Room for quick implementation

---
+++ template/src/Business/Pensiune/Room.cs
@@ -0,0 +1,20 @@
+﻿namespace Pensiune
+{
+    public abstract class Room
+    {
+        public decimal Price1day { get; set; }
+        public decimal Reservedays(int numberdays)
+        {
+            decimal reserveprice;
+            reserveprice = this.Price1day;
+            if (numberdays>3)
+            {
+                reserveprice = this.Price1day * 90 /100 ;
+
+            }
+           
+            return numberdays * reserveprice;
+        }
+
+    }
+}
