f8c9f44500742990bd4fdd7d51849a89daabba7b
roberto9985 <79300687+roberto9985@users.noreply.github.com>
2021-03-05 09:50:13 +0200

    Evaluare

---
+++ roberto9985/src/ev/GuestHouse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ev
+{
+    class GuestHouse {
+
+        List<room> ListOfRooms = new List<room>(6);
+ 
+
+        public GuestHouse(List<room> x)
+        {
+            for (int i = 1; i <=  6; ++i)
+            {   
+
+                ListOfRooms[i] = x[i];
+            }
+
+        }
+
+        public void reserve(TypeOfRoom)
+        {
+
+        }
+
+
+    }
+}
