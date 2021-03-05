4ca5f6263ef172f91ec8fac4c90bf79722eafd02
ionut ghimici <ionutghimici@yahoo.com>
2021-03-05 09:47:56 +0200

    Am creat camera dubla simpla.

---
+++ ionutg99/src/solutie/solutie/CameraDublaSimpla.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace solutie
+{
+    public  class CameraDublaSimpla:Camera
+    {
+        protected override decimal CalculeazaPretCamera(int n)
+        {
+            return 120m * n;
+        }
+    }
+}
