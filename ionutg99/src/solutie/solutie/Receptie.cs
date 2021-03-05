b036d6df114bcf1c2d1b7cb8b5bb04fdad3d9cc3
ionut ghimici <ionutghimici@yahoo.com>
2021-03-05 09:49:33 +0200

    Am creat receptia.

---
+++ ionutg99/src/solutie/solutie/Receptie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace solutie
+{
+    public class Receptie
+    {
+        public int nr_camere_ocupate=0;
+
+        public void Rezerva(Camera camera)
+        {
+            if (nr_camere_ocupate == 6)
+            {
+                throw new InvalidOperationException("Toate camerele sunt ocupate!");
+            }else if (camera.ocupat == true)
+            {
+                throw new InvalidOperationException("Camera este ocupata");
+            }
+            else
+            {
+                Console.WriteLine("Camera rezervata cu succes!");
+                nr_camere_ocupate++;
+            }
+        }
+    }
+}
