f3da28172007e2a9cfece132934439e9cdf53dfd
Benny99M <benescumatei@yahoo.com>
2021-03-05 09:38:16 +0200

    Fixed the simple calculations of decimal product and corrected the syntax errors

--- BenescuMatei_M533/src/BenescuMatei_App/BenescuMatei_App/Receptie.cs
+++ BenescuMatei_M533/src/BenescuMatei_App/BenescuMatei_App/Receptie.cs
@@ -15,30 +15,29 @@ namespace BenescuMatei_App
 
 
         };
-        public CalculeazaPret(Room camera, int durata, string optiune)
-        
+        public decimal CalculeazaPret(Room camera, int durata, string optiune)
+        {
 
-            return durata > 3 ? (menu[optiune] - menu[optiune] * 0.1) : menu[optiune];
+            decimal pret = menu[optiune];
+            return durata > 3 ? (pret - (pret * 0.1)) : pret;
 
 
 
         }
-                
 
-        public int CereMenu(Room camera)
+
+        public string CereMenu(Room camera)
         {
-        Console.WriteLine("Alegeti o camera la care doriti sa va cazati:\n");
-        Console.WriteLine("1: Camera de familie costă 200 RON/noapte");
-        Console.WriteLine("2: O cameră dublă simplă costă 120 RON/noapte");
-        Console.WriteLine("3:O cameră dublă cu vedere la munte costă 150 RON/noapte");
-        Console.WriteLine("4:O cameră la mansardă costă 100 RON/noapte");
-        int optiune= Console.ReadLine();
-        
-        if()
 
+            Console.WriteLine("Alegeti o camera la care doriti sa va cazati:\n");
+            Console.WriteLine("1: Camera de familie costă 200 RON/noapte");
+            Console.WriteLine("2: O cameră dublă simplă costă 120 RON/noapte");
+            Console.WriteLine("3:O cameră dublă cu vedere la munte costă 150 RON/noapte");
+            Console.WriteLine("4:O cameră la mansardă costă 100 RON/noapte");
+            string optiune = Console.ReadLine();
 
+            return optiune;
 
-    }
-            
+        }
     }
 }
