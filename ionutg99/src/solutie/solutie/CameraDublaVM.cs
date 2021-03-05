77a8d5efe39d7d115a2d357916ad1ef2604d682a
ionut ghimici <ionutghimici@yahoo.com>
2021-03-05 09:48:09 +0200

    Am creat camera dubla cu vedere la munte.

---
+++ ionutg99/src/solutie/solutie/CameraDublaVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace solutie
+{
+    public  class CameraDublaVM:Camera
+    {
+
+        protected override decimal CalculeazaPretCamera(int n)
+        {
+            return 150m * n;
+        }
+    }
+}
