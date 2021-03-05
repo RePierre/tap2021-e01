ad64cef6a5db3a988f21aac85f95fdcf3f6ea29b
ionut ghimici <ionutghimici@yahoo.com>
2021-03-05 09:49:03 +0200

    Am creat camera mansarda.

---
+++ ionutg99/src/solutie/solutie/CameraMansarda.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace solutie
+{
+    public  class CameraMansarda:Camera
+    {
+
+        protected override decimal CalculeazaPretCamera(int n)
+        {
+            if (n > 3)
+            {
+                return 100m * 3 + 90m*(n-3);
+            }
+            else
+            {
+                return 100m * n;
+            }
+        }
+    }
+}
