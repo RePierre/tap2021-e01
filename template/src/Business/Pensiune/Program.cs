20d79bc08172c4da80394ac50a4176137e6fa204
Mutescu Andrei-Ioan <amutescu@yahoo.com>
2021-03-05 09:12:42 +0200

    Implemented front to choose room type

---
+++ template/src/Business/Pensiune/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Pensiune
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string room;
+            Console.WriteLine("Choose one type of room: \n");
+            Console.WriteLine("1.Family Room \n" +
+                "2.Double Room \n" +
+                "3.Double Room + Mountain View \n" +
+                "4.Attic Room \n");
+
+            room = Console.ReadLine();
+
+        }
+    }
+}
