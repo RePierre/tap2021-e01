73158ad4f1149b3f5f397903a1341e50422d07bd
azbyn <andreypavel1@yahoo.com>
2021-03-05 09:18:18 +0200

    Populated Reception with rooms

--- azbyn/src/Reception.cs
+++ azbyn/src/Reception.cs
@@ -4,12 +4,39 @@ namespace src
 {
     public class Reception
     {
-        public readonly Reception _instance = new Reception();
+        public static readonly Reception _instance = new Reception();
 
-        public Reception Instance => _instance;
+        public static Reception Instance => _instance;
+
+        Room[] rooms;
 
         private Reception()
         {
+            rooms = new Room[] {
+                NewFamilyRoom(),
+
+                NewDoubleRoom(),
+                NewDoubleRoomWithMountainView(),
+                NewDoubleRoomWithMountainView(),
+
+                NewMansardRoom(),
+                NewMansardRoom(),
+            };
         }
+        public int CalculateCost(int roomNumber, DateTime start, DateTime end)
+        {
+            roomNumber--;
+            if (roomNumber < 0 || roomNumber >= rooms.Length)
+            {
+                throw new InvalidOperationException(
+                    $"Room {roomNumber + 1} doesn't exist. We only have rooms 1..{rooms.Length}");
+            }
+            return rooms[roomNumber].CalculateCost(start, end);
+        }
+        
+        private Room NewFamilyRoom() { return new Room(200, "Family Room"); }
+        private Room NewDoubleRoom() { return new Room(120, "Double Room"); }
+        private Room NewDoubleRoomWithMountainView() { return new Room(150, "Double Room With Mountain View"); }
+        private Room NewMansardRoom() { return new MansardRoom(100, "Mansard Room"); }
     }
 }
