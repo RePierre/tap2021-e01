c246f9cd81674cc4d2b9ec8bfef36bcdf110623a
Paraschiv Drobotă <54112866+ParaschivX@users.noreply.github.com>
2021-03-05 09:48:01 +0200

    Add files via upload
    Evaluare 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Receptie:\n");
+            Reception r1 = new Reception();
+            r1.ReceptionCall(1,5);
+            r1.ClearRoom(1);
+            Console.ReadKey();
+        }
+    }
+}
