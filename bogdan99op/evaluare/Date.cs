cdf0b747f64247b50a616c70dd93a09f3ff17807
Bogdan Andone <bogdan.andone11@gmail.com>
2021-03-05 09:37:47 +0200

    Created bew Date and Reception classes.Defined Date calss

---
+++ bogdan99op/evaluare/Date.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace evaluare
+{
+    class Date
+    {
+        private int day, month, year;
+        Date() { year = 0; month = 0; day = 0; }
+        Date(int y,int m,int d) { day = d;month = m;year = y; }
+        public int get_day() { return day; }
+        public int get_month() { return month; }
+        public int get_year() { return year; }
+    }
+}
