a3ec0c2f19bdc5c859077b1f7a17ee1b796a3f9e
Bogdan Andone <bogdan.andone11@gmail.com>
2021-03-05 09:08:22 +0200

    created new Room class

---
+++ bogdan99op/evaluare/Room.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace evaluare
+{
+    class Room
+    {
+        int floor, price, tenants;
+        //in floor retinem etajul;
+        //in price pretul,si tennants retinem umarul de chiriasi pe care il poate avea,astefel imi dau seama de ce fel e camera: daca tenants=2 inseamna ca e dubla,mai mare strict ca 2 inseamna ca e de familiee
+        bool nice_view;
+        //asa vad eu daca camera are vedere spre munte sau nu
+      
+    }
+}
