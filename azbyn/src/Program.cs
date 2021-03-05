85b580a9188b5000339e2cb906bc88b7571f2c8a
azbyn <andreypavel1@yahoo.com>
2021-03-05 09:35:19 +0200

    Added prints in Program.cs

--- azbyn/src/Program.cs
+++ azbyn/src/Program.cs
@@ -2,11 +2,20 @@
 
 namespace src
 {
-    public class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PrintPriceNicely(2, new DateTime(2021, 3, 5), new DateTime(2021, 3, 8));
+            PrintPriceNicely(1, new DateTime(2021, 3, 5), new DateTime(2021, 3, 6));
+            PrintPriceNicely(6, new DateTime(2021, 3, 5), new DateTime(2021, 3, 10));
+            PrintPriceNicely(5, new DateTime(2021, 3, 10), new DateTime(2021, 3, 12));
+        }
+        static void PrintPriceNicely(int roomNumber, DateTime start, DateTime end) {
+            double cost = Reception.Instance.CalculateCost(roomNumber, start, end);
+            var name = Reception.Instance.GetRoomName(roomNumber);
+            var delta = (end - start).Days;
+            Console.WriteLine($"Room {roomNumber} “{name}” costs {cost} RON for {delta} days");
         }
     }
 }
