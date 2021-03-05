180df6f3bca6df00968fbf9803402afe2851c5e0
ionut ghimici <ionutghimici@yahoo.com>
2021-03-05 09:47:18 +0200

    Am creat clasa Camera.

---
+++ ionutg99/src/solutie/solutie/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace solutie
+{
+    public abstract class Camera
+    {
+        public decimal pret { get; private set; }
+
+        public bool ocupat = false;
+
+        public decimal PretTotal(int n)
+        {
+            var calc = CalculeazaPretCamera(n);
+            pret += calc;
+            ocupat = true;
+            return pret;
+        }
+
+        protected abstract decimal CalculeazaPretCamera(int n);
+    }
+}
